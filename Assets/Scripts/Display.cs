5b1948d76f9e3b7068804d11af4042d3546fc459
dayvyn <davintx2001@aol.com>
2024-02-27 17:24:09 -0600

    Finished UI Visuals

---
+++ Assets/Scripts/Display.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class Display : MonoBehaviour
+{
+    [SerializeField] TMP_InputField[] letterBoxes;
+
+   void Start()
+    {
+
+    }
+
+
+
+
+    void Update()
+    {
+
+    }
+}
